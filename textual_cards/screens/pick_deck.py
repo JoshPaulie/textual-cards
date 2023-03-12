537df0bfc2a0d2a76f0d8b1130ad0fd14ad0b7af
joshpaulie <joshpaullee@gmail.com>
2023-03-12 12:06:43 -0500

    👷‍♂️ Functional list, no selection feat

--- textual_cards/screens/pick_deck.py
+++ textual_cards/screens/pick_deck.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from textual.screen import Screen
 from textual.widgets import ListItem, ListView, Static
 
@@ -12,9 +14,14 @@ class PickDeckScreen(Screen):
     """
 
     def compose(self):
-        self.decks_list_view = ListView
-        yield self.decks_list_view(
-            ListItem(Static("1")),
-            ListItem(Static("2")),
-            ListItem(Static("3")),
-        )
+        self.decks_list_view = ListView()
+        yield self.decks_list_view
+
+    def on_mount(self):
+        decks: pathlib.Path = pathlib.Path.home() / "decks"
+        for file in decks.rglob("*"):
+            self.decks_list_view.append(ListItem(Static(file.name)))
+        self.decks_list_view.focus()
+
+    def on_list_view_selected(self, item: ListView.Selected):
+        print(item.item)
