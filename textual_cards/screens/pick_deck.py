e99db10a9db41166ec3aa376357ac4836bdf9b11
joshpaulie <joshpaullee@gmail.com>
2023-03-15 20:09:11 -0500

    🤔 Begin blocking "no deck found" feat

--- textual_cards/screens/pick_deck.py
+++ textual_cards/screens/pick_deck.py
@@ -31,8 +31,19 @@ class PickDeckScreen(Screen):
         yield self.decks_list_view
 
     def on_mount(self):
-        decks: pathlib.Path = pathlib.Path.home() / "decks"
-        for file in decks.rglob("*"):
+        # check env var for alt path
+        # if so:
+        #   use that path
+        # if not:
+        decks_path: pathlib.Path = pathlib.Path.home() / "decks"
+
+        # push error screen if dir not found
+        # create the dir?
+        # push error screen if dir is found, but empty
+        # copy sample_deck?
+
+        decks = decks_path.rglob("*")
+        for file in decks:
             if file.name.startswith("."):
                 continue
             if not file.is_file():
@@ -41,6 +52,7 @@ class PickDeckScreen(Screen):
         self.decks_list_view.focus()
 
     # This gives me anxiety, it feels so hacky 😬
+    # todo - ref to dave's example on posting messages (https://gist.github.com/davep/630c827831fc283e14a657dee9add0a9)
     def on_list_view_selected(self, item):
         deck_path = item.item.deck_path
         # fyi I was unable to "query" the reactive element
